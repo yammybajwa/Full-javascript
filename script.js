01916064419601af9820e1c5dc5bf8f22b419d9f
yammybajwa <YAMMYBAJWA09@GMAIL.COM>
2022-01-31 21:45:50 +0500

    prompt and alert
    concept of also include if else

---
+++ script.js
@@ -0,0 +1,14 @@
+// alert
+
+// alert('welcome here!');
+
+//prompt with if else condition
+
+// const user = prompt();
+
+// if(user >= 18 ){
+//     alert("you are eligible");
+// } 
+// else{
+//     alert("you are not eligible");
+// };
