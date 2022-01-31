c841d860c71d58df3ad8705900d57b77dd3efaf5
yammybajwa <YAMMYBAJWA09@GMAIL.COM>
2022-01-31 22:39:18 +0500

    loops

--- script.js
+++ script.js
@@ -12,3 +12,32 @@
 // else{
 //     alert("you are not eligible");
 // };
+
+// const myName="muhammad";
+
+// for(let i=0 ; i<myName.length; i++){
+//     const result = myName;
+//     console.log(result)
+// };
+
+// const array = ["john","yammy","bajwa"]
+
+// for (let index=0; index < 20; index++){
+//     console.log(array)
+// };
+
+//table teller using loop and input
+
+// const btn = document.getElementById("submit");
+
+// btn.onclick =()=>{
+// const value = document.getElementById("value").value;
+
+
+
+//     for(let index=1; index < 11;index++){
+//        const result = value *index
+//       const finalResult = console.log( value+ " *", index + "=",result);
+//     }
+// } 
+ 
